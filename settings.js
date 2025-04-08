5771c5b4b7859886ec39ac716d811ff12fee13c7
Conway-eng <Mornah188@gmail.com>
2025-04-08 23:27:35 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,15 +1,15 @@
 /*I wrote a script to automate my job. Now I just sit back and watch Netflix while it runs.*/
 
-const session = process.env.SESSION || '';
+const session = process.env.SESSION || 'KEITH;;;H4sIAAAAAAAAA71W2Y6jOBT9F78mNWFfIpU0ZguQUCEL2Ubz4IAhJMEQMElIK/8+olKlLqmnq2uk1vBkGfvcc7dz/Q2QPK3wEDeg/w0UZXpGFLdL2hQY9IFWxzEuQRdEiCLQB8ORUPJRR/Yme9vdRL3wmiTnCV01y4qumTOzKRqjcw7mW3vyDO5dUNTbYxp+AujdXvaInBPYCW9JcDgynmyOX1zF6ZXaZrCcq+vrODVXS45VnsG9RURpmZLELHY4wyU6DnHjo7T8Gn3kzGOmtFS/Y53pTa/3m6tR8qhmBE0+bpcZpw72+2tgBUH1NfoDveNPZ2jU2Bx9iUJYjjUpqjMoz+N46I2v4iq3Q11XinnyoF+lCcGRE2FCU9p8Oe5Y3+yP214c7beVn17kxSnx9mfB2HCxVu9QqA7FATHCXoe7fI34mlSbAz8andF03VHheh47IyPPlcCSs9nCXaoXB5lz7YIn64/E/fK9Vg7/Je7euNjKp2XFbGtnP7IZrYLczhg3OuPdzAta8n5vHqs1tVZfpC+nBE7p1YvyHpedEubYm0RBFPnDzXQ8W/fcc9BbS7lmeZcP9BGty89YrsbNkZesTNyotd+EBZ1e3SN9MUbi+exEvjKHdLWogxLaHRdpV9LTMIvswymWQimRxOZ6TZxgPjKnQ8e5JZsqVwttlzy/enTAjROBPnvvghInaUVLRNOcvO4pfBeg6DzDYYnpa3jBeO174w20K+LporErysTWtsKOGC+Tfdrsq1LgbIcU+l5RnkEXFGUe4qrCkZ1WNC8bD1cVSnAF+n+9Zqp1usRZTrGbRqAPOFGQWUXhRZXn/6z+uOwQrVBR/EEwBV0Ql3nmYdCnZY274PWCzpsyqxkmp4pQNVhDFqHMm4bOQSgLjK60LmYPo/M0wxVFWQH6rCwIrMArDHPv/h4eliFaUDAtSzdMyZR1TZdYRhANWZYtqLLs/8VD5xRoSrrGaabIaJqoMwqEOmNqsqCzsql+zoOTfxcPSYS8YRqczkiSYfGMCTnF5CRTUEVLUIxfxIP/bTwYRTMNRpV5HZqWKlqKJQuGpcuMyTEGLzCf8xCY+99dQPCVPvSl7Qqe7YI4LSsakLo45ih6F5/3nygM85rQWUNCvV3gEvQ/bGNKU5JUrWc1QWW4S89Yb/0A/RgdK3zvggif0xC3eAAPoRJPIzqJ0Iy3YbrP5PEhaTtrl5PHkVASpCiU5Cc2jLZPQoTVJ1WQmCcRqVusyNuttI1AG46HtLd3fqo0ZDWsosJxz74UpfDGDJ2AHhR54pP8VS0eEoFLHL1HeYvCQ13M8wMmn+AWWDBSoYJhwPVGl9ksaTz3eqkPuRJ+wH1ID+h/+z5O9Txq8YYrOLJYTwJttlo7P1RDnxd+LAiC2tMgIAeSX0hr6C0LLUSEKUqPVds0vnyuK1Mzh+MRWyqDATQTqCcQfM/au0o/VBALGgvJSCLeZF2OfIFxF+oFJVl+XC/UF0ithYDxedrx9fz5X0DagBSc2wlzGPfi6Xy1ZoV8oV9PJ9bvnIh9Gk4jfFhmux5nsiNnIKC1u7ezptwpBDG5v16pqykfNry7TBY9sq7XegXzQL88t9Ye1fPRmM5i000azlBRdrFnQakuF8xGZn1hPzC9mO/x8+3KqZOblW8nlBFcJtsltrBc43hkkglL3GGQ2Tf+tAj8OikKukRO8jY/XufX8e3dkL4p+6PW4hS/juG3LPwyW9/Lnrl3P2C8DfaflJa2qmE0cZtOcTMHw+bWedEXcTjI3ZO7tOL5ZDRdYJuUTLy/nMC97efiiGicl1n77iJRmadtexxRReH3zv1RDGRV6IKsgUUxo4i+NzyA7ee4K3D/B7IF2USzCgAA';
 
-const prefix = process.env.PREFIX || '';
+const prefix = process.env.PREFIX || '#';
 const mycode = process.env.CODE || "254";
-const author = process.env.OWNER_NAME || 'Keith';
-const packname = process.env.PACKNAME || 'keith';
-const dev = process.env.OWNER_NUMBER || '254748387615';
+const author = process.env.OWNER_NAME || 'Siggtryggr';
+const packname = process.env.PACKNAME || 'Siggtryggr';
+const dev = process.env.OWNER_NUMBER || '254718835933';
 const DevKeith = dev.split(",");
 const botname = process.env.BOTNAME || 'KEITH-MD';
-const mode = process.env.MODE || 'public';
+const mode = process.env.MODE || 'private';
 const gcpresence = process.env.GC_PRESENCE || 'false';
 const antionce = process.env.ANTIVIEWONCE || 'true';
 const sessionName = "session";
