4a336f8a0adf55dd9733b769758f845f845a45fa
Conway-eng <Mornah188@gmail.com>
2025-04-11 01:22:06 +0300

    Gangstas

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 /*I wrote a script to automate my job. Now I just sit back and watch Netflix while it runs.*/
-const session = process.env.SESSION || 'KEITH;;;H4sIAAAAAAAAA5VVS46rRhTdSlRTu2P+GEstBYyxAf/BNibKoAwFrrYpMBQ2+MkLyDAryCRSRpln/jaUJUS0u9UtvU86SEilourcc+895/IJkBQXyEY16H0CWY7PkKJmSesMgR7QyihCOWiDEFIIesAe6cHFUgt0jp9G1bnes3ytlLx/EaL+HG52hXAkl51Pzxv1EdzaICt3Rxx8BzCUfLNKpqZaOR6ThuOJ2o+vM7mUQ26IRuTqMJOpUYcmYtNHcGsQIc4xiQfZHiUoh0cb1XOI84/RX9jHAT1ZutJaKqzjhiwZJmcvoVhcPsU+x7tdNt1gjqoc8zH6s3HXIAKXcbP10Esv2mFb2fohXbF76RpblTk1zX13eEBcrN7pFzgmKDRDRCim9YfrbqqLkW8IyDV9pq/kp2V0eoo68nC2tL2py3rX8W7LSMY+Gn2w7oE8mB9HzEwbdjpQIYZhZn1zcNA2E/3CyvlRMAu7RkfbG17eE5/nr1o5/J+6q7YbDLuOBensiTelSzGQXcXInPng6Ee7GlnaeLbUdUEgk4/Rt5Ts0jEvdjXz/O0Bz09h51TbfKQRonboOvTtZGh1hcmeMd/oQ1rm32N5GsF1PDpdnZUS1TW6yilTJmc+xoXqFVPsSGzQj3inuCp1XyfEX/bP132dGDPLbI2358l2KXe4QLEjp8rGuVtoLUN5ih+fMzqg2gxBj721QY5iXNAcUpySZk9k2gCGZwcFOaLP1QVPMhL9nTCvtsmeL/yMyHg5ZeCYC1dJhaHstGxOtmbTbFA8gjbI8jRARYHCES5omtcTVBQwRgXo/fzcqCbnHCUpRRYOQQ9woiAzXYkReFH4qfjxsoe0gFn2I0EUtEGUp8kEgR7NS9QGzxd02dB1TWaMgahKhi7qjNbVWVFnNdnQZVFqMkzuQV2coILCJAM9VhYETuFFRr790gYEVfQunyZpnm2DCOcFXZEyO6YwfNXW60cYBGlJqFOToN8sUA5677YRpZjERZNZSWAe7PEZ9Zs8QC+CxwLd2iBEZxygBg+wE2a3EoJseXUeTktYyJp+shZN5fYpuR+BkSyEkiA8IClkHwRODh4glKQHng8gy+2gCLsBaMpxd25z55tCYhbeqDxgTqk7vqbDQcBlSyONB1u7eBbDXQEoR+FrlXcwOJSZmx4Q+Q6ue1nLqhJrC3gY8tJ1teJN5HFzXtpu3+HelQV6n96mZT8NG7wFP9G6PLsBTbeaOF+oocd9qQcCm8PA/fx3VX/+6/OfP9iff/3n9z9++9rbsHhpUYMfIgrxsQA90Lc6djFLtcHUDni5Oxyqg1jtxyp4a+mrQ+8WuOCQiVp6fD3rVi7B0BdPibRbH/3xUljV4pynUVUri36gm49fAQE9gPIoZAa2f7XSctOZrYZkj8Trds6j6uotmY2YHLh8UYiMs1vsjbV1sDeaNly1ONKyT/7prJZyZUhZR5Qxy51PxZSEnrp4bKLdpfU+WMtKa+pBVxsx3c7AsmNN3LrTKOCmBFtXLU14JZ16mkKGlRjs4nJc2Xq6NcXcyarNnEB/QD23Fsx1ah8m/vCyzsddrN5nx/PsOr78M/CLre9CjDB6HsEvPfqvVr5Zgrm130G8zPRvyE4LTBKOOrEwkS81bCVkg2BWJqy/cRhkTEpXkb1ucF6pRK3BrfF6doQ0SvOk8RUJ8xSHoA2OsKDqm6u/HBSCzLVBUqtZ5lBIX4cBUJvHdlvg9i9fyQQ0rggAAA=='
-const prefix = process.env.PREFIX || '.';
+const session = process.env.SESSION || 'KEITH;;;H4sIAAAAAAAAA61V2W6rSBD9l361M2ZfLEUadgev2MbbaB4aaDA2m5sGjK/87yPiRIl072Qy0uUJNfSpU1XnVP0AWR6XaIxaMPwBChzXkKDulbQFAkOgVmGIMOiDABIIhsCyj/N6u50ffDbEEd7Xkqu7e29QGPzkIngHkjlQ8OL5fDt9Bvc+KCovif0vABE1gNbuqlqH2MlP4UjccvZ8ZsYm0sdLcjotTscpf0iQ5/jP4N4hwhjHWWQUR5QiDJMxahcwxt+jL+hHhonyJNvLWcFfzZHNGEyVTFC+YPOxRAnLjKIGwcY1ne/Rl69luFY9cWpu/eC0GLgCaRV+LjqVVepVVJU1JUmBqGdn90G/jKMMBS8BykhM2m/X3bXNoL01gShN8Dhxl64d3YwVU0zcQXGyt9Hi4pwmKXt52ey/R3ymTZVeq1vMi38jnqpVqbR0mR21hNcI1qemzeesl69HN/X8mfgCv2vl/H/q3k7V69XS08kob47n3azSj6t2MN7xsZ/viQgR5tNUnx8c85v0R8c2PQatpcrjXTKAjYAP22w5pyinHt3C7FzsFY7kS5U9ch/0IanwVyz1YpkdxMlYdg5qTc0OGDUL2IjUmL9oyhofbi+aC3WfZQVe2u9FZNMjKJD1vNqcRHOqNbMFJdPMzd6FTIM2AY7hpVGV59eMzqh9CcCQvvcBRlFcEgxJnGfdGSf1AQzqFfIxIq/VBTbl6ZooW0usyUoTSWJ0EHshdV6NaMsns3ZkUPtKYv2LTD2DPihw7qOyRMEoLkmO2ykqSxihEgz/em1UlzNGaU6QHQdgCBieE2lJYnmZZf8s/2iOkJSwKP7IEAF9EOI8nSIwJLhCffB6QZQFiRJEimYVlaJZSVMYipEFgWNFwdQVvcswfQRdxykqCUwLMKRFjmMZlpXEe//38BAYTmcMlZUMnZUoheYYQ1VMVhZEWTBkUf0PHtLv4kHRCi8rHKupnMGyrKlyMi8JjKIYqkmbgvQlD46hfhcPUaU1hWE4mTVlWWJFzRB1XqUYSVEEyaCEr3mw1P3vPsjQlTxs3YmRpfsgjHFJ3KwqkhwG755//wh9P68ysmozX+teEAbDT8eIkDiLyi6zKoPYP8Y10ro8wDCESYnufRCgOvZRhwdUzpO4ST1fPz01TCLMglYJF1Gn6GOePX7xQ4qWkCg/BQzjPXGBxDxBFIhPCCIkhwET+GwAunI8Jmp3518N7hRFK0v8YFynI5OeKj01XlaX+nTrKa8mfTgTYRS8V9mD/rkq1vkZZV/gTtymdHQ10FR4yTaD9Wq1XjU9c769OZ9wH44Hwx8fW0zLgw5vsmKXqk3zoOtWF+cnNQx5+mdBZLD7G7jZOcubrAv01oUOIkAExkkJhkB7uTVVaajGGFmcJ1mWYkSKFingo2vvw/ExfRCn0ko2EbKps8eTBUfZG7mBUZon+408U4i54RCql72Flj//AqRbi5a+2E1ZW9QFr9UZIkZ7EbpaMm+PlHsVGmOQOoyWWLXW7jhe8Rt91Go2HzMXmRMwWUNHbRLn4vOeVO9Ye6CtVgfdee6iPdTzOZg7ejEcHF+47XTtC/B8radq2zuau5UzYKbuqEgJ0xts0G5VaXm+ud0wESbJcbPTdWbjLZ0lt2nZwblnkfNhjVtBi6aOFj3G9uvaSN7Wdfw2UR9aC2P0uv3euvCf3fqQPXXvf8J426f/Ii11VymBY7e94mZY4/bWm2mb0Ldy+2JvzXDtTJYbNMowFZ6aC7h3fi4SSMIcp2AIYBbgPO7skcCSKB/O/dVwpPsgbZWiWBFI3g0PlO55CShw/weCtop6KgoAAA==';
+const prefix = process.env.PREFIX || '#';
 const mycode = process.env.CODE || "254";
 const author = process.env.OWNER_NAME || 'Siggtryggr';
 const packname = process.env.PACKNAME || 'Siggtryggr';
@@ -19,17 +19,17 @@ const gurl = process.env.GURL || 'https://chat.whatsapp.com/DM1tZs0C6LJ8Sxrhm6Qw
 const reactemoji = process.env.EMOJI || '';
 const antitag = process.env.ANTITAG || 'true';
 const groupControl = process.env.GROUP_CONTROL || 'true';
-const anticall = process.env.ANTICALL || 'false';
+const anticall = process.env.ANTICALL || 'true';
 const antidelete = process.env.ANTIDELETE || 'false';
 const antibot = process.env.ANTIBOT || 'false';
 const autoview = process.env.AUTOVIEW_STATUS || 'true';
-const autolike = process.env.AUTOLIKE_STATUS || 'true';
+const autolike = process.env.AUTOLIKE_STATUS || 'false';
 const chatbot = process.env.CHATBOT || 'false';
 const greet = process.env.GREET || 'true';
 const autodownloadstatus = process.env.AUTODOWNLOAD_STATUS || 'false';
 const autostatusreply = process.env.AUTOREPLY_STATUS || 'false';
 const autostatusmsg = process.env.AUTOSTATUS_MSG || 'viewed';
-const greetmsg = process.env.GREET_MSG || 'nice to chat with you  ';
+const greetmsg = process.env.GREET_MSG || 'currently unavailable please try aga later  ';
 const timezone = process.env.TIMEZONE || 'Africa/Nairobi';
 const autoread = process.env.AUTOREAD || 'true';
 const permit = process.env.PM_PERMIT || 'true';
